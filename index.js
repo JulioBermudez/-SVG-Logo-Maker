d872ef28509a1583a6b4966afb3ccf4d95493e16
Julio Bermudez <julioph0n3@gmail.com>
2023-08-23 12:42:47 -0400

    added functionability

---
+++ index.js
@@ -0,0 +1,61 @@
+const Triangle = require("./lib/Triangle");
+const Circle = require("./lib/Circle");
+const Square = require("./lib/Square");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Enter SVG text:",
+      name: "text",
+    },
+    {
+      type: "list",
+      message: "Enter the color of the text",
+      name: "textColor",
+      choices: ["red", "blue", "green", "yellow","white"],
+    },
+    {
+      type: "list",
+      message: "Enter the Shape",
+      name: "shape",
+      choices: ["circle", "square", "triangle"],
+    },
+    {
+      type: "list",
+      message: "Enter the shape color",
+      name: "shapeColor",
+      choices: ["red", "blue", "green", "yellow"],
+    },
+  ])
+  .then((response) => {
+    if (response.shape === "circle") {
+      const circle = new Circle();
+      circle.setColor(response.textColor);
+      circle.setText(response.text);
+      circle.setShapeColor(response.shapeColor);
+      fs.writeFile("./examples/logo.svg", circle.render(), (err)=>{
+        console.log("Generated Logo.svg");
+      })
+    }else
+    if (response.shape === "square") {
+      const square = new Square();
+      square.setColor(response.textColor);
+      square.setText(response.text);
+      square.setShapeColor(response.shapeColor);
+      fs.writeFile("./examples/logo.svg", square.render(), (err)=>{
+        console.log("Generated Logo.svg");
+      })
+    }else
+    if (response.shape === "triangle") {
+      const triangle = new Triangle();
+      triangle.setColor(response.textColor);
+      triangle.setText(response.text);
+      triangle.setShapeColor(response.shapeColor);
+      fs.writeFile("./examples/Logo.svg", triangle.render(), (err)=> {
+        console.log("Generated Logo.svg");
+      })
+    }
+  });
